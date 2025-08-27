02bea0d3073a527fd458a33b4ab9480d859f5048
Utkarsh <iutkarsh24mishra@gmail.com>
2025-08-27 13:09:21 +0530

    requirement installations done

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md","r",encoding="utf-8") as f:
     long_description = f.read()
 
-__version ="0.0.0"
+__version__ ="0.0.0"
 
 REPO_NAME = "Text_Summarizer_Project"
 AUTHOR_USER_NAME = "Utkarsh"
